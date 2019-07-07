9cee0ce8f94c6f9277fbe7f55a08f7359f0ddf3d
azidanit <azidan.it@gmail.com>
2019-07-07 20:30:48 +0700

    mencoba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Mahmud zehan
                 </div>
 
                 <div class="links">
