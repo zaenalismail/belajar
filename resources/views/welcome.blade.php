cc29809862b2297743528f87b26a375bc344bab6
azidanit <azidan.it@gmail.com>
2019-07-07 20:38:10 +0700

    merge coba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Mahmud zehan
+                    Mahmudsadsadsadsa dsad
                 </div>
 
                 <div class="links">
